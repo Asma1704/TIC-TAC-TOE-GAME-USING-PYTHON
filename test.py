a631a7b19e9f47d567ae60c11e2bb141e2279e46
Asma1704 <160950824+Asma1704@users.noreply.github.com>
2024-02-23 01:26:49 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,61 @@
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 5)
+
+
+def check_winner(board):
+    for row in board:
+        if row.count(row[0]) == len(row) and row[0] != ' ':
+            return True
+
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] and board[0][col] != ' ':
+            return True
+
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != ' ':
+        return True
+
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != ' ':
+        return True
+
+    return False
+
+
+def is_board_full(board):
+    for row in board:
+        for cell in row:
+            if cell == ' ':
+                return False
+    return True
+
+
+def play_tic_tac_toe():
+    board = [[' ' for _ in range(3)] for _ in range(3)]
+    current_player = 'X'
+
+    while True:
+        print_board(board)
+        row = int(input(f"Player {current_player}, choose row (0, 1, 2): "))
+        col = int(input(f"Player {current_player}, choose column (0, 1, 2): "))
+
+        if board[row][col] != ' ':
+            print("That cell is already taken. Try again.")
+            continue
+
+        board[row][col] = current_player
+
+        if check_winner(board):
+            print_board(board)
+            print(f"Player {current_player} wins!")
+            break
+
+        if is_board_full(board):
+            print_board(board)
+            print("It's a tie!")
+            break
+
+        current_player = 'O' if current_player == 'X' else 'X'
+
+
+play_tic_tac_toe()
